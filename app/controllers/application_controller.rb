105ec2e75f1e41b0a52c35923645c5d3a27e6cdd
kyyk5447 <w140113@gmail.com>
2025-05-18 20:30:04 +0900

    キャメルケースで受け取ったリクエストをスネークケースに変換する処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
   protect_from_forgery with: :exception
+  before_action :transform_params_to_snake_case
 
   protected
 
@@ -22,5 +23,54 @@ class ApplicationController < ActionController::API
     return if member_signed_in?
 
     render_unauthorized
+  end
+
+  private
+
+  def transform_params_to_snake_case
+    params.keys.each do |key|
+      next unless key.match?(/[A-Z]/)
+      snake_key = camelize_to_snake(key)
+      
+      # キャメルケースの値を取得し、スネークケースに変換
+      camel_value = params.delete(key)
+      if camel_value.is_a?(ActionController::Parameters) || camel_value.is_a?(Hash)
+        snake_value = deep_transform_keys_to_snake(camel_value.to_unsafe_h)
+      else
+        snake_value = camel_value
+      end
+
+      # スネークケースのキーに値をセット
+      params[snake_key] = snake_value
+    end
+
+    # ネストされたパラメータも変換
+    params.each do |key, value|
+      if value.is_a?(ActionController::Parameters) || value.is_a?(Hash)
+        params[key] = deep_transform_keys_to_snake(value.to_unsafe_h)
+      end
+    end
+  end
+
+  def deep_transform_keys_to_snake(hash)
+    hash.each_with_object({}) do |(key, value), result|
+      snake_key = camelize_to_snake(key)
+      result[snake_key] = case value
+        when Hash
+          deep_transform_keys_to_snake(value)
+        when Array
+          value.map { |v| v.is_a?(Hash) ? deep_transform_keys_to_snake(v) : v }
+        else
+          value
+        end
+    end
+  end
+
+  def camelize_to_snake(key)
+    key.to_s
+      .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+      .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+      .tr('-', '_')
+      .downcase
   end
 end
