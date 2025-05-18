9b96c08d5e2dfb1e96b07feb942e20774c39361d
kyyk5447 <w140113@gmail.com>
2025-05-18 17:36:50 +0900

    もとに戻す

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# キャメルケース変換のカスタムミドルウェアを読み込み
-require_relative '../app/middleware/camel_case_transformer'
-
 Bundler.require(*Rails.groups)
 
 module App
@@ -15,10 +12,8 @@ module App
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
-    config.session_store :cookie_store, key: '_ec_site_session'
+    config.session_store :cookie_store, key: '_your_app_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
-    config.middleware.use CamelCaseTransformer
-    config.hosts << 'rails-ec-rest-api.onrender.com' if Rails.env.production?
   end
 end
